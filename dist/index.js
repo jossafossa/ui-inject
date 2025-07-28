17382d8efa302ce329723a0651d49399f974a185
Joost Hobma <joosthobma@gmail.com>
2025-07-28 15:01:11 +0200

    Auto-commit on save

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Github Enhancer
 // @namespace     http://tampermonkey.net/
-// @version       1753707658791
+// @version       1753707671051
 // @description   Enhance your Github experience with additional features.
 // @author        Jossafossa
 // @match         https://**/*
@@ -10,8 +10,8 @@
 // @grant         GM_setValue
 // @grant         GM_getValue
 // @grant         GM_registerMenuCommand
-// @downloadURL   https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
-// @updateURL     https://raw.githubusercontent.com/jossafossa/github-inject/refs/heads/master/dist/index.js
+// @downloadURL   https://raw.githubusercontent.com/jossafossa/ui-inject/refs/heads/master/dist/index.js
+// @updateURL     https://raw.githubusercontent.com/jossafossa/ui-inject/refs/heads/master/dist/index.js
 // ==/UserScript==
 
 (function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c=".extended_filter{resize:horizontal;overflow:auto}",s=document.createElement("style");s.innerHTML=c;document.head.appendChild(s);
