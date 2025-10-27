8ae1d8c2cbe79015fbfae1ae0914a46ad7a668eb
Joost Hobma <joosthobma@gmail.com>
2025-10-27 10:53:15 +0100

    Auto-commit on save

--- src/messages.ts
+++ src/messages.ts
@@ -18,9 +18,9 @@ export const init = () => {
         "backdrop",
         "header",
         "export",
-        "endpointPending",
-        "endpointRejected",
-        "endpointFulfilled",
+        "reduxPending",
+        "reduxRejected",
+        "reduxFulfilled",
     ]
 
     // Listen for messages
